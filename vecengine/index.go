0b67543d13952ee72d73a9deec2e16581004b8d3
sergey <msgcloud2@gmail.com>
2023-11-08 15:09:20 +0300

    logs

--- vecengine/index.go
+++ vecengine/index.go
@@ -227,7 +227,7 @@ func (vi *Engine) fillEventVectors(e dag.Event) (allVecs, error) {
 	// store calculated vectors
 	vi.callback.SetHighestBefore(e.ID(), myVecs.before)
 	vi.callback.SetLowestAfter(e.ID(), myVecs.after)
-	fmt.Println("SET EVENt BRANCH ID", e.ID(), meBranchID)
+	fmt.Println("SET EVENt BRANCH ID", e.ID(), meBranchID, &vi)
 	vi.SetEventBranchID(e.ID(), meBranchID)
 
 	return myVecs, nil
